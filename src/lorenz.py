54018b64d2e8a863b5caf8717a90352656d18ae4
Rahul Sajnani <rahulsajnani2010@hotmail.com>
2021-04-11 22:55:51 +0530

    Add critical points

--- src/lorenz.py
+++ src/lorenz.py
@@ -15,14 +15,23 @@ class Lorenz_equations:
         
 
     def getCriticalPoints(self):
-        pass
+        '''
+        Get critical points for Lorenz equation
+        dX/dt = 0
+        '''
+        
+        p_0 = np.array([0, 0, 0])
+        p_1 = np.array([np.sqrt(self.beta * (self.rho - 1)),  np.sqrt(self.beta * (self.rho - 1)), self.rho - 1])
+        p_2 = np.array([-np.sqrt(self.beta * (self.rho - 1)),  -np.sqrt(self.beta * (self.rho - 1)), self.rho - 1])
+
+        return {"p_0": p_0, "p_1": p_1, "p_2": p_2}
 
     def getLorenzMatrix(self, x, y, z):
         '''
         Get Lorenz matrix dX/dt = AX
         
-        dx/dt = -sigma * x + sigma * y + 0 * y
-        dy/dt = (rho + z/2) * x + (- 1) * y + (x/2) * z = rho * x -y - xz
+        dx/dt = -sigma * x + sigma * y + 0 * z
+        dy/dt = (rho - z/2) * x + (- 1) * y - (x/2) * z = rho * x -y - xz
         dz/dt = (y/2) * x + (x/2) * y - (beta) * z = xy - beta * z
         '''
 
@@ -42,6 +51,7 @@ class Lorenz_equations:
         
         trajectory = [X]
         for i in range(num_points):
+            
             delta_X = A @ X * self.delta_t
             X = delta_X + X
             A = self.getLorenzMatrix(X[0], X[1], X[2])
@@ -55,17 +65,23 @@ class Lorenz_equations:
         '''
         Plot the lorenz trajectory given initial point
         '''
+        critical_points = self.getCriticalPoints()
         trajectory = self.getLorenzTrajectory(initial_point, num_points)
 
-        # Plot
-        ax = plt.figure().add_subplot(projection='3d')  
-        
-        ax.plot(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2])
+        ax = plt.figure().add_subplot(projection='3d')          
+        for p in critical_points:
+            point = critical_points[p]
+            if not np.iscomplex(point).all():
+                ax.scatter3D(point[0], point[1], point[2])
+
+        ax.plot3D(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2])
         plt.show()
+
 
 if __name__ == "__main__":
 
-    lorenz = Lorenz_equations(prandtl_number = 10, rayleigh_number = 28, beta = 10/3, delta_t = 1e-2)
+    lorenz = Lorenz_equations(prandtl_number = 10, rayleigh_number = 25, beta = 10/3, delta_t = 1e-2)
     lorenz.plotLorenzTrajectory([1, 2, 0])
-
+    critical_points = lorenz.getCriticalPoints()
+    print(critical_points)
         
